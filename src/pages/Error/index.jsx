058e799e4ecd01fd27678b15e9ad3e0174b44c4f
wolf011 <duckhouse_bousoukis@aleeas.com>
2025-06-11 22:11:55 -0300

    estrutura primitiva do trabalho

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <div>
+        <h1>404 - Error, page not found</h1>
+    </div>
+  )
+}
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div>
+        <h1>Página de login</h1>
+    </div>
+  )
+}
