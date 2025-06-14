3586c56bb20cfe8583c7e988040a2f870e9941d2
Maria Vitória Martelli <martellimv@gmail.com>
2025-06-13 21:24:16 -0300

    adição do modo escuro

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState, useContext} from "react";
+
+const ThemeContext = createContext();
+
+export default function ThemeProvider({children}) {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleTheme = () => setDarkMode(prev => !prev);
+
+  return (
+    <ThemeContext.Provider value={{darkMode, toggleTheme}}>
+        {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export const useTheme = () => useContext(ThemeContext);
